6a998485c4cd586e94b3f0eb7990d4755b9fd386
Piero Viano <piero.viano@gmail.com>
2022-12-16 12:35:27 +0200

    ''

--- Build.Debug.bat
+++ Build.Debug.bat
@@ -1,7 +1,7 @@
 del Artifacts\Debug\Net4x.*
 for /d %%G in ("%userprofile%\.nuget\Packages\Net4x.*") do rd /s /q "%%~G"
 nuget restore CuteAnt.Extensions.sln
-MSBuild.exe CuteAnt.Extensions.sln /property:Configuration=Debug
+MSBuild.exe CuteAnt.Extensions.sln -m /property:Configuration=Debug
 git push
 git add -A
 git commit -a --allow-empty-message -m ''
