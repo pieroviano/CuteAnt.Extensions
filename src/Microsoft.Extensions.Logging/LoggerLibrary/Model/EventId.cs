e660fc75c1fffedfd12b92deac299039b2b1c610
Piero Viano <piero.viano@gmail.com>
2022-12-22 08:40:05 +0200

    Added net35 logger

---
+++ src/Microsoft.Extensions.Logging/LoggerLibrary/Model/EventId.cs
@@ -0,0 +1,20 @@
+﻿namespace Microsoft.Extensions.Logging
+{
+    public struct EventId
+    {
+        private int _id;
+        private string? _name;
+
+        public EventId(int id, string? name = null)
+        {
+            this._id = id;
+            this._name = name;
+        }
+
+        public int Id => this._id;
+
+        public string? Name => this._name;
+
+        public static implicit operator EventId(int i) => new EventId(i);
+    }
+}
