8a48db654d12c39909353fb16d0cea03568441d6
Piero Viano <piero.viano@gmail.com>
2023-04-30 06:40:09 +0300

    ''

--- Build.bat
+++ Build.bat
@@ -1,9 +1,14 @@
 IF NOT DEFINED Configuration SET Configuration=Release
-md %USERPROFILE%\.nuget\packages\system.buffers\4.4.0\lib\netstandard1.1
-echo >%USERPROFILE%\.nuget\packages\system.buffers\4.4.0\lib\netstandard1.1\System.Buffers.dll
+if exist  Properties\Lock.txt del Properties\Lock.txt
+if not exist "%USERPROFILE%\.nuget\packages\system.buffers\4.4.0\lib\netstandard1.1\" md "%USERPROFILE%\.nuget\packages\system.buffers\4.4.0\lib\netstandard1.1"
+type NUL >%USERPROFILE%\.nuget\packages\system.buffers\4.4.0\lib\netstandard1.1\System.Buffers.dll
+if not exist "%USERPROFILE%\.nuget\packages\system.buffers\4.4.0\lib\netstandard1.1\" md "%USERPROFILE%\.nuget\packages\system.buffers\4.4.0\lib\netstandard2.0"
+type NUL >%USERPROFILE%\.nuget\packages\system.buffers\4.4.0\lib\netstandard2.0\System.Buffers.dll
+del Properties/Lock.txt
 MSBuild.exe Net4x.Extensions.sln -t:clean
 MSBuild.exe Net4x.Extensions.sln -t:restore -p:RestorePackagesConfig=true /property:Configuration=%Configuration%
 MSBuild.exe Net4x.Extensions.sln -m /property:Configuration=%Configuration%
+del Properties/Lock.txt
 IF DEFINED Package (
 	cd Packages
 	FOR %%i IN (Net4x.AspNetCore.JsonPatch.*.nupkg) DO dotnet nuget push %%i --source NuGet --api-key %ApiKey% -t 1000
